d4756afc33cd0fcf491553e56146095694edcd8a
tusharrevankar <tusharrevankar@gmail.com>
2024-02-16 08:12:19 +0530

    ecom

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "i.ibb.co",
+      "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com",
+      "img.freepik.com", 
+      "images.pexels.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
